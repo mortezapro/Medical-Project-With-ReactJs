2eeb855e2c22abde6f5f68277c6f8ba54abd4151
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2022-11-26 11:36:07 +0330

    installing and config tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-sky-200">
+      <h1 className="text-center text-sky-600 text-3xl font-bold pt-5">
+        Hello World!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
