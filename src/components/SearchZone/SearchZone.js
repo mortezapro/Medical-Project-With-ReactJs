2169a827e5f9f4b475279c92e36045a04008c0b1
mortezapro <goodarzi.morteza73@gmail.com>
2022-12-01 13:43:50 +0330

    images

---
+++ src/components/SearchZone/SearchZone.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
+
+export default class SearchZone extends React.Component {
+    render() {
+        return (
+            <div className="container mx-auto">
+                <div className="grid pt-4 lg:grid-cols-4 md:grid-cols-2">
+                    <div className=" lg:pl-2 md:pl-2 mb-4">
+                        <input type="text" placeholder="جستجو ..." className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primary focus:shadow"/>
+                    </div>
+                    <div className="lg:px-2 md:pr-2 mb-3">
+                        <select className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primary focus:shadow">
+                            <option defaultValue disabled hidden>دسته بندی پدر</option>
+                            <option >دسته اول</option>
+                            <option >دسته دوم</option>
+                        </select>
+                    </div>
+                    <div className="lg:px-2 md:pl-2 mb-3">
+                        <select className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primary focus:shadow">
+                            <option defaultValue disabled hidden>دسته بندی فرزند</option>
+                            <option >دسته اول</option>
+                            <option >دسته دوم</option>
+                        </select>
+                    </div>
+                    <div className="lg:pr-2 md:pr-2 mb-3">
+                        <button type="button" className="btn btn-primary w-full">
+                            <MagnifyingGlassIcon className="w-6 h-6 inline-block ml-1"/>
+                            جستجو
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
