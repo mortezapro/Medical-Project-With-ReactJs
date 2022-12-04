6b5d0b8ca3ad09024d4512c2f3cac780e28b976d
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2022-12-04 17:59:07 +0330

    add login page route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Home from "./pages/home/Home";
-import {Route, Routes} from "react-router-dom";
+import { Login } from "./pages";
+import { Route, Routes } from "react-router-dom";
 const App = () => {
-    return (
-        <>
-            <Routes>
-                <Route exact path="/" element={<Home/>} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
