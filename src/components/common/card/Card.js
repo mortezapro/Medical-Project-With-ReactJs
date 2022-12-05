41bb9e065a0114ac75019c2320b136dd018e3c8a
mortezapro <goodarzi.morteza73@gmail.com>
2022-12-05 20:22:12 +0330

    fix bug

--- src/components/common/card/Card.js
+++ src/components/common/card/Card.js
@@ -8,7 +8,7 @@ export default function Card(props){
             </div>
             <div className="bg-white p-4 pb-10 shadow-sm leading-7 dark:bg-dark-secondary dark:color-secondary dark:border dark:border-gray-600">
                 <h2 className="font-bold mb-3">{props.title}</h2>
-                <p className="font-light color-dark-secondary">{props.summary}</p>
+                <p className="font-light">{props.summary}</p>
             </div>
             <div className="block text-center">
                 <Link className="bg-primary color-secondary px-6 py-2 relative top-[-12px] rounded-[12px]" to={props.link}>
