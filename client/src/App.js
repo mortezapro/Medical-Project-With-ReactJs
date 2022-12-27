fb8cbf0347c1887a857beebac0f5e73824488051
askari-mahsa <mahsa.ba.askari@gmail.com>
2022-12-27 12:01:07 +0330

    category page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import { Home, Login } from "./pages";
+import { Category } from "pages/Category/Category";
 import { Route, Routes } from "react-router-dom";
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/category" element={<Category />} />
+			</Routes>
+		</>
+	);
 };
 
 export default App;
